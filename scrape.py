e4af914b2fd9b7b2c83e522269539a4992cc8734
Eric Njeru <ericmukundi10@gmail.com>
2022-12-14 10:28:22 +0300

    use headless chrome

--- scrape.py
+++ scrape.py
@@ -483,7 +483,7 @@ def parse_args():
 
 def get_chrome_options():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument("--test-type")
     chrome_options.add_argument("--no-sandbox")
